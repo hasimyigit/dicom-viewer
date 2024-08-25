e0a6b1546e8464940769239cc07c11c9059ae94e
Haşim Yiğitgülsün <hasimyigitt@gmail.com>
2024-08-26 02:53:36 +0300

    remove aria-hidden attribute

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -13,7 +13,6 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
   return (
     <div
       id="default-modal"
-      aria-hidden="true"
       className="flex bg-slate-950 bg-opacity-30 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-[calc(100%-1rem)] max-h-full"
     >
       <div className="relative p-4 w-full max-w-2xl max-h-full">
